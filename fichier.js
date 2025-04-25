7e748abac939dfe42597d0e6653375f672041f9d
antso <randrianasoloantso5@gmail.com>
2025-04-25 12:08:32 +0200

    créé moi un site web

--- fichier.js
+++ fichier.js
@@ -1,6 +1,7 @@
 // Qu'est ce que tu aimerais mettre dans cette fichier
 // mettre des variables dans ce fichier
-let firstname = "nomena";
+//fais moi un site web qui saisit demande le nom d'utilisateur
+let firstname = document.getElementById("This_is_a_test");
 let greetings = `Hello ${firstname}`
 let secondName= "Antso";
 let salutation="bonjour "+secondName;
